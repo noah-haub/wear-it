8583ae9924d036f1df49baa528946fa0520a2580
luismm24 <luis@mvst.co>
2024-11-20 10:25:43 +0100

    add tailwind to the project

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      borderRadius:{
+
+      },
+      colors:{
+        offWhite: '#E1E1E1',
+      }
+    },
+
+  },
+  plugins: [],
+}
+
